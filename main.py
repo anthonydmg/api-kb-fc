ce148fd80bd828e1342cea8b74a17f2e411e52ac
Anthony Miranda <anthonymirandagil@gmail.com>
2024-04-27 18:11:04 -0500

    []

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 @app.get("/")
 def read_root():
@@ -9,3 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
